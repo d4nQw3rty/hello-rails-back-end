0bc8c6a7684b8fb3f26185d8444838e5436552b8
d4nQw3rty <danlinmoy@gmail.com>
2023-02-22 18:41:54 -0400

    create api

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,9 +1,10 @@
 class GreetingsController < ApplicationController
   before_action :set_greeting, only: %i[ show edit update destroy ]
 
-  # GET /greetings or /greetings.json
+   # GET /greetings or /greetings.json
   def index
-    @greetings = Greeting.all
+    @greetings = Greeting.all.sample
+    render json: @greetings, status: 200
   end
 
   # GET /greetings/1 or /greetings/1.json
